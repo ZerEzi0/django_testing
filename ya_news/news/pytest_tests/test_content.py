eeb70fb22f9bf6bb5be2f0dfec9c8e7d45b3c7c1
Игорь Честа <igorche2760@gmail.com>
2024-09-25 02:25:02 +0300

    pep8 update

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -4,11 +4,15 @@ from django.contrib.auth.models import User
 from news.models import News, Comment
 from django.utils import timezone
 
+
 @pytest.mark.django_db
 class TestYaNewsContent:
     @pytest.fixture(autouse=True)
     def setup(self, client):
-        self.user1 = User.objects.create_user(username='user1', password='password1')
+        self.user1 = User.objects.create_user(
+            username='user1',
+            password='password1'
+        )
 
         # Creating news articles
         for i in range(12):
@@ -19,34 +23,56 @@ class TestYaNewsContent:
             )
         
         self.news = News.objects.first()
-        self.comment1 = Comment.objects.create(news=self.news, author=self.user1, text="User1's Comment")
+        self.comment1 = Comment.objects.create(
+            news=self.news,
+            author=self.user1,
+            text="User1's Comment"
+        )
 
     def test_main_page_contains_no_more_than_10_news(self, client):
-        response = client.get(reverse('news:home'))
+        response = client.get(
+            reverse('news:home')
+        )
         assert response.status_code == 200
         assert len(response.context['news_list']) <= 10
 
-    def test_news_are_sorted_by_date(self, client):
-        response = client.get(reverse('news:home'))
+    def test_news_are_sorted_by_date(
+            self,
+            client
+        ):
+        response = client.get(
+            reverse('news:home')
+        )
         news_list = response.context['news_list']
-        # Проверим, что первая дата не меньше последней, если они равны, тест пройдет
         assert news_list[0].date >= news_list[len(news_list)-1].date
 
     @pytest.mark.skip(reason="Comments not found in context")
     def test_comments_are_sorted_by_oldest_first(self, client):
-        response = client.get(reverse('news:detail', kwargs={'pk': self.news.id}))
+        response = client.get(
+            reverse(
+                'news:detail',
+                kwargs={'pk': self.news.id}
+        ))
         assert response.status_code == 200
         comments = response.context.get('comments', None)
         assert comments is not None, "No comments found in context"
         assert comments[0].created < comments[-1].created
 
     def test_anonymous_user_cannot_see_comment_form(self, client):
-        response = client.get(reverse('news:detail', kwargs={'pk': self.news.id}))
+        response = client.get(
+            reverse(
+                'news:detail',
+                kwargs={'pk': self.news.id}
+        ))
         assert response.status_code == 200
         assert 'form' not in response.context
 
     def test_authenticated_user_can_see_comment_form(self, client):
         client.login(username='user1', password='password1')
-        response = client.get(reverse('news:detail', kwargs={'pk': self.news.id}))
+        response = client.get(
+            reverse(
+                'news:detail',
+                kwargs={'pk': self.news.id}
+        ))
         assert response.status_code == 200
         assert 'form' in response.context
