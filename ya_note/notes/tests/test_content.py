65d370bf02c52006073406390fa87710e14077b5
Игорь Честа <igorche2760@gmail.com>
2024-09-25 02:11:18 +0300

    PEP8 update

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -1,19 +1,28 @@
 import pytest
-import unittest
-from django.test import Client
+from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
 from notes.models import Note
 from notes.forms import NoteForm
 
 @pytest.mark.django_db
-class YaNoteContentTestCase(unittest.TestCase):
+class YaNoteContentTestCase(TestCase):
     def setUp(self):
         self.client = Client()
-        self.user1 = User.objects.create_user(username='user1', password='password1')
-        self.user2 = User.objects.create_user(username='user2', password='password2')
-        self.note1 = Note.objects.create(title="User1's Note", text="Text for user1", slug="user1-note", author=self.user1)
-        self.note2 = Note.objects.create(title="User2's Note", text="Text for user2", slug="user2-note", author=self.user2)
+        self.user1 = User.objects.create_user(
+            username='user1', password='password1'
+        )
+        self.user2 = User.objects.create_user(
+            username='user2', password='password2'
+        )
+        self.note1 = Note.objects.create(
+            title="User1's Note", text="Text for user1",
+            slug="user1-note", author=self.user1
+        )
+        self.note2 = Note.objects.create(
+            title="User2's Note", text="Text for user2",
+            slug="user2-note", author=self.user2
+        )
 
     def test_note_in_object_list(self):
         self.client.login(username='user1', password='password1')
@@ -28,14 +37,8 @@ class YaNoteContentTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.context['form'], NoteForm)
 
-        response = self.client.get(reverse('notes:edit', kwargs={'slug': self.note1.slug}))
+        response = self.client.get(reverse('notes:edit', kwargs={
+            'slug': self.note1.slug
+        }))
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.context['form'], NoteForm)
-
-    def test_notes_are_filtered_by_user(self):
-        self.client.login(username='user1', password='password1')
-        response = self.client.get(reverse('notes:list'))
-        self.assertEqual(response.status_code, 200)
-        notes = response.context['object_list']
-        self.assertIn(self.note1, notes)
-        self.assertNotIn(self.note2, notes)
